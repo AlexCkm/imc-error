c4b178fef392741e4010e32a18029672f1f336b8
Alumno <alex@gmail.com>
2022-07-19 20:27:06 +0200

    commit error parte tres

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -8,46 +8,62 @@ import com.example.imc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var height = 150
+    private var weight = 75
+    private var doubleheight = 2.25
+    private var imc = 33.33
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        resultado()
 
 
-            binding.sbPeso.setOnSeekBarChangeListener(object:
-                SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(seekbar: SeekBar,
+        binding.sbAltura.setOnSeekBarChangeListener(object:
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekbar: SeekBar?,
                                                progress: Int,
                                                fromUser: Boolean) {
-                    binding.tvPeso2.text = seekbar.progress.toString().plus("/200")
-                }
+                binding.tvAltura2.text = progress.toString().plus("/200")
+                height = progress
+            }
 
 
-                override fun onStartTrackingTouch(seek: SeekBar?) {
-                    //write custom code for progress is starting
-                }
+            override fun onStartTrackingTouch(seek: SeekBar?) {
+            }
 
-                override fun onStopTrackingTouch(seek: SeekBar?) {
-                    //write custom code for progress is stopped
+            override fun onStopTrackingTouch(seek: SeekBar?) {
+                resultado()
                 }
             })
-
-            binding.sbAltura.setOnSeekBarChangeListener(object:
+        binding.sbPeso.setOnSeekBarChangeListener(object:
                 SeekBar.OnSeekBarChangeListener{
-                override fun onProgressChanged(seekbar: SeekBar,
+                override fun onProgressChanged(seekbar: SeekBar?,
                                                progress: Int,
                                                fromUser: Boolean) {
-                    binding.tvAltura2.text = seekbar.progress.toString().plus("/200")
+                    binding.tvPeso2.text = progress.toString().plus("/150")
                 }
                 override fun onStartTrackingTouch(seek: SeekBar?) {
-                    //write custom code for progress is starting
+
                 }
 
                 override fun onStopTrackingTouch(seek: SeekBar?) {
-                    //write custom code for progress is stopped
+                    resultado()
                 }
             })
+
         }
+    fun resultado(){
+        doubleheight = height.times(height)/1000.0
+        imc =Math.round((weight/doubleheight)
+            .times(100))
+            .div(100.0)
+        binding.tvresultado.text = imc.toString()
+        calcObesidad()
+    }
+    fun calcObesidad(){
+
+    }
 }
 
 
