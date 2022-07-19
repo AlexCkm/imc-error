f24a6f83614a1d01afb25b27c3baf5725ae98199
Alumno <alex@gmail.com>
2022-07-19 16:25:34 +0200

    commit error parte dos

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import com.example.imc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+            binding.sbPeso.setOnSeekBarChangeListener(object:
+                SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekbar: SeekBar,
+                                               progress: Int,
+                                               fromUser: Boolean) {
+                    binding.tvPeso2.text = seekbar.progress.toString().plus("/200")
+                }
+
+
+                override fun onStartTrackingTouch(seek: SeekBar?) {
+                    //write custom code for progress is starting
+                }
+
+                override fun onStopTrackingTouch(seek: SeekBar?) {
+                    //write custom code for progress is stopped
+                }
+            })
+
+            binding.sbAltura.setOnSeekBarChangeListener(object:
+                SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(seekbar: SeekBar,
+                                               progress: Int,
+                                               fromUser: Boolean) {
+                    binding.tvAltura2.text = seekbar.progress.toString().plus("/200")
+                }
+                override fun onStartTrackingTouch(seek: SeekBar?) {
+                    //write custom code for progress is starting
+                }
+
+                override fun onStopTrackingTouch(seek: SeekBar?) {
+                    //write custom code for progress is stopped
+                }
+            })
+        }
 }
+
+
